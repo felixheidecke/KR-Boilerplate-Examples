ff8874c1240dec67aeb6fb0fd88b300dfbf0dc66
Felix Heidecke <felix@heidecke.me>
2023-11-04 14:12:03 +0100

    Remove demo comment

--- artikel/+page.js
+++ artikel/+page.js
@@ -1,19 +1,6 @@
 import { getArticles } from '$lib/boilerplate/libraries/xioni-cms/src/Articles'
 import xioniLoader from '$lib/boilerplate/utils/xioni-loader'
 
-export const load = async () => {
-	const articles = await xioniLoader(getArticles(1383))
-
-	/* Artikelinhalte komplett übergeben */
-	return {
-		articles
-	}
-
-	/* Allen artikel die "website" entziehen 
-	return {
-		articles: articles.map(article => {
-			return { ...article, website: undefined }
-		})
-	}
-	*/
-}
+export const load = async () => ({
+	articles: await xioniLoader(getArticles(1383))
+})
