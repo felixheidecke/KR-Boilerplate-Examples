a4ddbeb47656a9fc7ae06e71e595768a098a5292
Felix Heidecke <felix@heidecke.me>
2023-11-18 21:38:58 +0100

    5.1.5.Beta

--- oeffnungszeiten/+page.svelte
+++ oeffnungszeiten/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { format } from '$lib/boilerplate/utils/formatDate'
+	import { format } from '$lib/utils/formatDate'
 	import { isWithinInterval } from 'date-fns'
 
 	const periodStartDate = new Date('2023-06-05')
