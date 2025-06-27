4d1e44e523dde4fbe6845f2317be87f9ce2c8dc6
Felix Heidecke <felixheidecke@users.noreply.github.com>
2025-06-27 14:24:51 +0200

    Add Addressbook Module (#2)

---
+++ addressbook/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import AddressbookEntry from '$lib/boilerplate/components/AddressbookEntry'
+	import Client from '$lib/boilerplate/components/Client/Client.svelte'
+
+	export let data
+</script>
+
+<h1>Ortsvereine</h1>
+
+<Client browser>
+	{#each data.categories as category}
+		<h2>{category.title}</h2>
+
+		<ul class="$flex $flex-column $gap">
+			{#each category.entries || [] as entry}
+				<li>
+					<AddressbookEntry {entry} />
+				</li>
+			{/each}
+		</ul>
+	{/each}
+</Client>
