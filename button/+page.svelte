5eaed026fe40dc658f3009199341ed4220bd3541
Felix Heidecke <felix@heidecke.me>
2025-10-31 13:07:55 +0100

    Add YouTube video component and update button layout in button page

--- button/+page.svelte
+++ button/+page.svelte
@@ -1,23 +1,60 @@
 <script lang="ts">
-	import { Button, ButtonRow, Wrapper } from '$lib/boilerplate/components'
+	import { Button, ButtonRow } from '$lib/boilerplate/components'
 </script>
 
-<Wrapper size="smaller">
-	<h1>Button</h1>
+<h1>Button</h1>
 
-	<ul>
-		<li><Button>Hallo</Button></li>
-		<li class="$mt"><Button fontello="attention-circled" class="$row-reverse">Hinweis</Button></li>
-		<li class="$mt"><Button fontello="youtube-play">YouTube</Button></li>
-	</ul>
+<ul>
+	<li><Button>Hallo</Button></li>
+	<li class="$mt"><Button fontello="attention-circled" class="$row-reverse">Hinweis</Button></li>
+	<li class="$mt"><Button fontello="youtube-play">YouTube</Button></li>
+</ul>
 
-	<hr />
+<hr />
 
-	<h2 class="$my-3">ButtonRow</h2>
+<h2 class="$my-3">ButtonRow</h2>
 
-	<ButtonRow>
-		<Button>Hallo</Button>
-		<Button>Hallo</Button>
-		<Button>Hallo</Button>
-	</ButtonRow>
-</Wrapper>
+<ButtonRow>
+	<Button>Standard</Button>
+	<Button variant="primary">Primär</Button>
+	<Button variant="secondary">Sekundär</Button>
+</ButtonRow>
+
+<code class="$mt-4"
+	><pre>{`<ButtonRow>
+  <Button>Standard</Button>
+  <Button variant="primary">Primär</Button>
+  <Button variant="secondary">Sekundär</Button>
+</ButtonRow>`}</pre></code>
+
+<style>
+	code {
+		position: relative;
+		display: block;
+		padding: 1rem;
+		background-color: aliceblue;
+		border-bottom-left-radius: 0.5rem;
+		border-bottom-right-radius: 0.5rem;
+		outline: 1px solid rgb(210, 234, 255);
+		outline-offset: -1px;
+	}
+
+	code::before {
+		position: absolute;
+		top: 0;
+		left: 0;
+		transform: translateY(-100%);
+		content: 'Code:';
+		font-size: small;
+		padding: 0.75rem;
+		width: 100%;
+		background-color: rgb(210, 234, 255);
+		border-top-left-radius: 0.5rem;
+		border-top-right-radius: 0.5rem;
+	}
+
+	code pre {
+		margin: 0;
+		line-height: 175%;
+	}
+</style>
---
+++ youtube/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Youtube } from '$lib/boilerplate/components'
+</script>
+
+<h1>YouTube Video</h1>
+
+<Youtube videoId="A7eE5o2LVm4" style="width: 600px" />
