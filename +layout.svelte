e216ee1ffe9aefe51019ab8ce2fcec1220dc828f
Felix Heidecke <felix@heidecke.me>
2025-12-03 12:10:11 +0100

    Add layout and styling for code examples in layout.svelte and button page

---
+++ +layout.svelte
@@ -0,0 +1,40 @@
+<script>
+	const { children } = $props()
+</script>
+
+{@render children()}
+
+<style global>
+	:global(code.example) {
+		position: relative;
+		display: block;
+		padding: 1rem;
+		background-color: aliceblue;
+		border-bottom-left-radius: 0.5rem;
+		border-bottom-right-radius: 0.5rem;
+		outline: 1px solid rgb(210, 234, 255);
+		outline-offset: -1px;
+		font-size: 90%;
+		margin-top: calc(38px + 1rem);
+		margin-bottom: 1rem;
+	}
+
+	:global(code.example::before) {
+		position: absolute;
+		top: 0;
+		left: 0;
+		transform: translateY(-100%);
+		content: 'Code:';
+		font-size: 12px;
+		padding: 12px;
+		width: 100%;
+		background-color: rgb(210, 234, 255);
+		border-top-left-radius: 0.5rem;
+		border-top-right-radius: 0.5rem;
+	}
+
+	:global(code.example pre) {
+		margin: 0;
+		line-height: 200%;
+	}
+</style>
---
+++ link/+page.svelte
@@ -0,0 +1,61 @@
+<script>
+	import { Link } from '$lib/boilerplate/components'
+</script>
+
+<h1>Link</h1>
+
+<p>URLs beginnend mit <em>http</em> werden immer im neuen Tab geöffnet.</p>
+
+<ul>
+	<li><Link to="https://klickrhein.de" /></li>
+	<li><Link to="https://klickrhein.de">Klickrhein im Rheingau</Link></li>
+</ul>
+
+<code class="example"
+	><pre>{`<Link to="https://klickrhein.de" />
+<Link to="https://klickrhein.de">Klickrhein im Rheingau</Link>`}</pre></code>
+
+<h2>Mit target</h2>
+
+<p>Mittels target kann das selbst gesteuert werden.</p>
+
+<ul>
+	<li><Link to="https://klickrhein.de" target="_self">Öffnet hier</Link></li>
+	<li><Link to="/button" target="_blank">Öffnet in neuem Tab</Link></li>
+</ul>
+
+<code class="example"
+	><pre>{`<Link to="https://klickrhein.de" target="_self">Klickrhein öffnet hier</Link>
+<Link to="/button" target="_blank">Öffnet in neuem Tab</Link>`}</pre></code>
+
+<h2>Mit Icons</h2>
+
+<p>Links können mit Fontello-Icons versehen werden.</p>
+
+<ul>
+	<li><Link to="https://facebook.com" fontello="facebook">Facebook</Link></li>
+	<li><Link to="https://instagram.com" fontello="instagram">Instagram</Link></li>
+	<li><Link to="mailto:info@example.com" fontello="mail">E-Mail senden</Link></li>
+	<li><Link to="tel:+49123456789" fontello="phone">Anrufen</Link></li>
+</ul>
+
+<code class="example"
+	><pre>{`<Link to="https://facebook.com" fontello="facebook">Facebook</Link>
+<Link to="https://instagram.com" fontello="instagram">Instagram</Link>
+<Link to="mailto:info@example.com" fontello="mail">E-Mail senden</Link>
+<Link to="tel:+49123456789" fontello="phone">Anrufen</Link>`}</pre></code>
+
+<h2>Mit aria-label</h2>
+
+<p>Für bessere Barrierefreiheit können aria-label gesetzt werden.</p>
+
+<ul>
+	<li><Link to="/impressum" label="Zur Impressum-Seite navigieren">Impressum</Link></li>
+	<li>
+		<Link to="https://example.com" fontello="link" label="Externe Website besuchen">Website</Link>
+	</li>
+</ul>
+
+<code class="example"
+	><pre>{`<Link to="/impressum" label="Zur Impressum-Seite navigieren">Impressum</Link>
+<Link to="https://example.com" fontello="link" label="Externe Website besuchen">Website</Link>`}</pre></code>
